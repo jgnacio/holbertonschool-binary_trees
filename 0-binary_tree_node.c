b4ad9658171957d487ee03e8a1bfb14bbf80610b
CarolinaMolinaSosa <molinasosacarolina17@gmail.com>
2023-01-11 06:19:16 -0800

    Add a first excercise, function that creates a binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a new node
+ * @parent: the parent of the nodes
+ * @value: value of the nodes
+ *
+ * Return: the newnode
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+
+	newnode->parent = parent;
+	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
+
+	return (newnode);
+}
