b6eadb49e9d14163165949cd7637d7206b034c45
CarolinaMolinaSosa <molinasosacarolina17@gmail.com>
2023-01-11 06:24:02 -0800

    fixed Malloc

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,6 +10,8 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
 
 	newnode->parent = parent;
 	newnode->n = value;
