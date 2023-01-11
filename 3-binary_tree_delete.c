78e44dd0ba25f2bc3b2c58c6e29ce727b6ba6b10
CarolinaMolinaSosa <molinasosacarolina17@gmail.com>
2023-01-11 08:56:05 -0800

    function that deletes an entire binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delate node
+ * @tree: data
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
+	tree = NULL;
+}
