e4c21a040e08e9a5ea3fa4834652f52775907527
jgnacio <jgnaciogomez@gmail.com>
2023-01-11 07:47:13 -0800

    fix NULL case for parent node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
 
-	if (!newnode)
+	if (!newnode | !parent)
 		return (NULL);
 
 	if (parent->left)
