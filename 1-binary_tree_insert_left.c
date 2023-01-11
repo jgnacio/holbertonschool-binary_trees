dfdd72a9d6b6d28731003f60d8a7dc4f673bc8a7
jgnacio <jgnaciogomez@gmail.com>
2023-01-11 08:19:23 -0800

    fix declaration variable wrong

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,10 +9,12 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newnode = NULL;
+
 	if (!parent)
 		return (NULL);
 
-	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+	newnode = malloc(sizeof(binary_tree_t));
 	if (!newnode)
 		return (NULL);
 
