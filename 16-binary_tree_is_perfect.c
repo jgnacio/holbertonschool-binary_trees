bbf62191722c5fd1789bdf41c110f70901a714f9
jgnacio <jgnaciogomez@gmail.com>
2023-01-13 09:09:16 -0800

    fix when a tree is complete but dont have the same count of leaves

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 #include "9-binary_tree_height.c"
-#include "15-binary_tree_is_full.c"
+#include "12-binary_tree_leaves.c"
 
 /**
  * binary_tree_is_perfect - fuction that check if a tree is perfect
@@ -11,7 +11,9 @@
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int l_height = 0,
-			r_height = 0;
+			l_leaves = 0,
+			r_height = 0,
+			r_leaves = 0;
 
 	l_height = binary_tree_height(tree->left);
 	r_height = binary_tree_height(tree->right);
@@ -19,5 +21,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (l_height != r_height)
 		return (0);
 
-	return (binary_tree_is_full(tree));
+	l_leaves = binary_tree_leaves(tree->left);
+	r_leaves = binary_tree_leaves(tree->right);
+
+	return (l_leaves == r_leaves);
 }
